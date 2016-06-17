ed1f748aaaeb250c10c22a46b4c22933f98a0095
Abhijeet Tallavajhula <abhijeet.t91@gmail.com>
2016-06-17 16:36:14 -0400

    Shifted out mini crusher.

--- mini_crusher/serial_api/include/serial_api/Test.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-namespace serial_api {
-    class Test {
-    public:
-        bool testBuild();
-    };
-} // namespace serial_api
--- mini_crusher/serial_api/src/Test.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <serial_api/Test.h>
-
-using namespace serial_api;
-
-bool Test::testBuild()
-{
-    return true;
-}
